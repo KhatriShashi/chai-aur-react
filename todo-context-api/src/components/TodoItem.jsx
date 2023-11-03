ca0e3a6d71839fed039354a92f52595c52e73ca3
KhatriShashi <shashikrkhatri9546@gmail.com>
2023-11-03 09:47:23 +0530

    change css in markdone

--- todo-context-api/src/components/TodoItem.jsx
+++ todo-context-api/src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useTodo } from '../context/TodoContext';
 
-function TodoItem({ todo}) {
+function TodoItem({ todo }) {
     const [isTodoEditable, setIsTodoEditable] = useState(false);
     const [todoMsg, setTodoMsg] = useState(todo.text)
     const { updateToDo, removeToDo, markToDo } = useTodo();
@@ -15,17 +15,17 @@ function TodoItem({ todo}) {
     }
     const editPriority = () => {
         console.log(todo.priority);
-        let newPriority=todo.priority==="low" ? "high" : "low";
+        let newPriority = todo.priority === "low" ? "high" : "low";
         console.log(newPriority);
         const updatedTodo = {
-          ...todo,
-          id: Date.now(),
-          priority: newPriority,
+            ...todo,
+            id: Date.now(),
+            priority: newPriority,
         };
-      
+
         updateToDo(todo.id, updatedTodo);
-      };
-      
+    };
+
     // updateToDo(todo.id,{...todo,id:Date.now(),priority:!isPriority})
     // setIsPriority(!isPriority);
 
@@ -35,12 +35,32 @@ function TodoItem({ todo}) {
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
                 }`}
         >
-            <input
+            <div className="checkbox-wrapper-12">
+                <div class="cbx">
+                    <input id="cbx-12" type="checkbox" className="cursor-pointer"
+                checked={todo.completed}
+                onChange={markedTodo} />
+                    <label for="cbx-12"></label>
+                    <svg width="15" height="14" viewbox="0 0 15 14" fill="none">
+                        <path d="M2 8.36364L6.23077 12L13 2"></path>
+                    </svg>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                        <filter id="goo-12">
+                            <fegaussianblur in="SourceGraphic" stddeviation="4" result="blur"></fegaussianblur>
+                            <fecolormatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" result="goo-12"></fecolormatrix>
+                            <feblend in="SourceGraphic" in2="goo-12"></feblend>
+                        </filter>
+                    </defs>
+                </svg>
+            </div>
+            {/* <input
                 type="checkbox"
                 className="cursor-pointer"
                 checked={todo.completed}
                 onChange={markedTodo}
-            />
+            /> */}
             <input
                 type="text"
                 className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
@@ -57,7 +77,7 @@ function TodoItem({ todo}) {
                     editPriority();
                 }}
             >
-                {todo.priority==="high" ? <i class="ri-star-fill text-yellow-200 text-lg"></i> : <i class="ri-star-line text-lg"></i>}
+                {todo.priority === "high" ? <i class="ri-star-fill text-yellow-200 text-lg"></i> : <i class="ri-star-line text-lg"></i>}
             </button>
             {/* Edit, Save Button */}
             <button
